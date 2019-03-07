39bc3a32729a35b9bcac4bc1f51b5ce63008c68b
zhangsan <huhlxulg@163.com>
2019-03-07 23:14:28 +0800

    create project

---
+++ ideaOne/src/com/sanjiu/TestOne.java
@@ -0,0 +1,10 @@
+package com.sanjiu;
+
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+    }
+}
